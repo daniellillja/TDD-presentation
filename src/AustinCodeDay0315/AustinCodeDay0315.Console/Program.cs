61131191f99b4a45012bf2b2ca73afbb0344f296
DANIEL.LILLJA <daniel.lillja@cuc.org>
2015-03-24 22:04:09 -0500

    Test #2: OnePlusTwoEqualsThree

---
+++ src/AustinCodeDay0315/AustinCodeDay0315.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AustinCodeDay0315.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ src/AustinCodeDay0315/AustinCodeDay0315.Tests/StringCalculator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+
+namespace AustinCodeDay0315.Tests
+{
+    // Any object that allows you to add given a string expression
+    public interface IStringCalculator
+    {
+        int Add(string numbers);
+    }
+
+    // StringCalculator must implement all methods declared in interface
+  
+    public class StringCalculator:IStringCalculator
+    {
+        // What must our calculator do? We need to be able to add numbers from an input string
+        public int Add(string numbers)
+        {
+            if (String.IsNullOrWhiteSpace(numbers))
+            {
+                throw new ArgumentException("Expression should not be null or empty.");
+            }
+
+            var sum = 0;
+            var symbols = numbers.Split('+');
+            foreach (var symbol in symbols)
+            {
+                int parsed = 0;
+                Int32.TryParse(symbol, out parsed);
+                sum += parsed;
+            }
+
+            return sum;
+        }
+    }
+
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        // Let's start with the simplest test case - what happens when a user enters an empty string?
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EmptyStringShouldThrowErrorOnAdd()
+        {
+            // Arrange - setup your test
+            var fixture = new StringCalculatorTestFixture();
+            var sut = fixture.BuildSut();
+            var input = string.Empty;
+
+            // Act - lets see what the sut (system under test) returns to us
+            var output = sut.Add(input);
+
+            // Assert - what should happen under these conditions?
+            // not necessary - we have covered our assert with the ExpectedExceptionAttribute above
+
+        }
+
+        [TestMethod]
+        // Lets do a successful test case - adding two numbers
+        public void OnePlusTwoEqualsThree()
+        {
+            // Arrange
+            var fixture = new StringCalculatorTestFixture();
+            var sut = fixture.BuildSut();
+            var input = "1+2";
+
+            // Act
+            var output = sut.Add(input);
+
+            // Assert
+            // Lets try a cleaner way of asserting - introducting the Should library
+            output.ShouldEqual(3);
+
+
+        }
+
+    }
+
+
+    public class StringCalculatorTestFixture
+    {
+        public StringCalculator StringCalculator { get; set; }
+
+        public StringCalculatorTestFixture()
+        {
+            StringCalculator = new StringCalculator();
+        }
+
+        public StringCalculator BuildSut()
+        {
+            return new StringCalculator();
+        }
+    }
+}
